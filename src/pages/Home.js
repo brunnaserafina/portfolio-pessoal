7114557a4b6606fafff91953cc3f648a657204a0
Brunna Serafina <brunnaserafina@gmail.com>
2023-07-25 23:57:57 -0300

    style: updated favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { keyframes, styled } from "styled-components";
 import { AiFillGithub, AiFillLinkedin, AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoLogoWhatsapp } from "react-icons/io";
-import logo from "../assets/images/Logo6.png";
+import logo from "../assets/images/Logo5.png";
 import person from "../assets/images/Logo nav.png";
 
 export default function Home({
